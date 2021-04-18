4bdee7c9f02508daab1664a7e97a70f37d03061b
yuqiwangyuqi <wangyuqi033@gmail.com>
2021-04-18 10:56:58 -0700

    user profile page implemented

--- dandd/src/components/Profile.js
+++ dandd/src/components/Profile.js
@@ -1,12 +1,44 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import {useParams} from "react-router-dom";
 
 function Profile() {
-    return (
-      <div >
+  const { user_id } = useParams();
+  const url = `https://6079bca0460a6600174fc355.mockapi.io/profile/${user_id}`;
+  const [profile, setProfile] = useState(null);
 
+  let content = null;
+
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setProfile(response.data)
+      })
+  }, [url])
+
+  if(profile) {
+    content =
+      <div className="container">
+        <div className="user_profile">
+          <div className="upper_container">
+            <div className="img_container">
+              <img src="https://icons-for-free.com/iconfiles/png/512/agenda+app+contacts+online+profile+user+icon-1320183042135412367.png" alt='' height="100px" width="100px"/>
+            </div>
+          </div>
+          <div className="lower_container">
+            <h2>User ID: {profile.user_id}</h2>
+            <h2>Username: {profile.username}</h2>
+            <h3>Email: {profile.email}</h3>
+          </div>
+        </div>
       </div>
-    );
+  }
+
+  return (
+    <div>
+      {content}
+    </div>
+  )
 }
 
 export default Profile;
